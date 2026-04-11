e6b7e16308347c7c6e5d2d486b5f226af8b58556
minminchoco <silverlh614@gmail.com>
2026-04-11 09:20:14 +0900

    refactor: quantEngine에서 직접 export를 index 모듈로 변경하여 하위 호환성 유지
    index.ts 작성 후 quantEngine.ts 최종 교체
    typescript// src/services/quant/index.ts
    export * from './gateEngine';
    export * from './evolutionEngine';
    export * from './technicalEngine';
    export * from './bearEngine';
    export * from './sectorEngine';
    export * from './ipsEngine';
    export * from './fssEngine';
    quantEngine.ts 최종 형태 (20줄로 완성):
    typescript/**
     * @deprecated 직접 import 대신 ./quant/* 서브모듈을 사용하세요.
     * 하위 호환성을 위해 re-export를 유지합니다.
     */
    export * from './quant/index';

---
+++ src/services/quant/index.ts
@@ -0,0 +1,7 @@
+export * from './gateEngine';
+export * from './evolutionEngine';
+export * from './technicalEngine';
+export * from './bearEngine';
+export * from './sectorEngine';
+export * from './ipsEngine';
+export * from './fssEngine';
--- src/services/quantEngine.ts
+++ src/services/quantEngine.ts
@@ -1,32 +1,5 @@
-export {
-  ALL_CONDITIONS,
-  SELL_CHECKLIST,
-  CONDITION_SOURCE_MAP,
-  getEvolutionWeightsFromPerformance,
-  saveEvolutionWeights,
-} from './quant/evolutionEngine';
-
-export {
-  computeConfluence, classifyCyclePosition, gradeCatalyst,
-  analyzeMomentumAcceleration, evaluateTMA, evaluateSRR,
-  evaluateEnemyChecklist, computeDataReliability, computeSignalVerdict,
-} from './quant/technicalEngine';
-
-export {
-  evaluateGate0, evaluateMAPCResult, getFXAdjustmentFactor,
-  getRateCycleAdjustment, getStockProfile, computeContrarianSignals,
-  classifyExtendedRegime, deriveExtendedRegime, evaluateStock,
-  detectROETransition,
-} from './quant/gateEngine';
-
-export {
-  evaluateBearSeasonality, evaluateBearRegime,
-  evaluateInverseGate1, evaluateVkospiTrigger,
-  evaluateMarketNeutral, evaluateBearScreener,
-  evaluateBearKelly, evaluateBearModeSimulator,
-} from './quant/bearEngine';
-
-export { evaluateIPS } from './quant/ipsEngine';
-
-export { classifyForeignSupplyDay, computeFSS } from './quant/fssEngine';
-export { evaluateSectorOverheat } from './quant/sectorEngine';
+/**
+ * @deprecated 직접 import 대신 ./quant/* 서브모듈을 사용하세요.
+ * 하위 호환성을 위해 re-export를 유지합니다.
+ */
+export * from './quant/index';
