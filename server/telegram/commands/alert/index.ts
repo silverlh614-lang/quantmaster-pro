ff4ee71efc95ea231378c0bd51de9a068a211b01
Minminchoco <silverlh614@gmail.com>
2026-05-10 22:50:54 +0900

    PATCH-ALERT register channel routes command

--- server/telegram/commands/alert/index.ts
+++ server/telegram/commands/alert/index.ts
@@ -1,8 +1,9 @@
 // @responsibility index 텔레그램 모듈
-// @responsibility: commands/alert/* 9 cmd 자동 등록 트리거 (channel/alert/dxy/news/digest 9 entries with aliases = 12 keys).
+// @responsibility: commands/alert/* cmd 자동 등록 트리거 (channel/alert/dxy/news/digest entries).
 import './alertHistory.cmd.js';
 import './alertReplay.cmd.js';
 import './channelHealth.cmd.js';
+import './channelRoutes.cmd.js';
 import './channelStats.cmd.js';
 import './channelTest.cmd.js';
 import './digest.cmd.js';
